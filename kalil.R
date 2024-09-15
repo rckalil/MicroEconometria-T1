b3f9d34e133aa2074fa588b970f168f814faf2c6
rckalil <b46837@fgv.edu.br>
2024-09-15 16:00:20 -0300

    Impacto

--- kalil.R
+++ kalil.R
@@ -17,4 +17,46 @@ dados_3 <- subset(dados, GRUPO == 3)
 dados_3
 print(summary(dados_3))
 
-# sws
+# No grupo 3, escolhido, 2004 mulheres participaram da pesquisa
+# A renda familiar per capita média foi de 443.1
+
+tempo0 <- subset(dados_3, dados_3$tempo == 0)
+tempo1 <- subset(dados_3, dados_3$tempo == 1)
+
+escolaridade <- lm(escolaridade ~ tratado, data = tempo0)
+print('Escolaridade no tempo 0')
+print(summary(escolaridade))
+
+idade <- lm(idade ~ tratado, data = tempo0)
+print('Idade no tempo 0')
+print(summary(idade))
+
+luz <- lm(luz ~ tratado, data = tempo0)
+print('Luz no tempo 0')
+print(summary(luz))
+
+renda_pc <- lm(renda_pc ~ tratado, data = tempo0)
+print('Renda per capita no tempo 0')
+print(summary(renda_pc))
+
+# O parâmetro que diferencia o grupo tratado do grupo de controle é pequeno em relação ao intercepto
+# Isso indica que há pouca diferença entre os grupos, o que é um bom sinal
+
+tratado0 <- subset(dados_3, dados_3$tratado == 0)
+tratado1 <- subset(dados_3, dados_3$tratado == 1)
+
+impacto <- lm(renda_pc ~ tempo, data = tratado1)
+print('Impacto do tratamento')
+print(summary(impacto))
+
+# O parâmetro de tempo é significativo, indicando que o tratamento teve impacto na renda per capita
+
+impacto <- lm(renda_pc ~ tratado, data = tempo1)
+print('Impacto do tratamento')
+print(summary(impacto))
+
+# O parâmetro de tratado é significativo, indicando que o tratamento teve impacto na renda per capita
+
+impacto <- lm(renda_pc ~ tratado + idade + luz + escolaridade, data = tempo1)
+print('Impacto do tratamento')
+print(summary(impacto))
